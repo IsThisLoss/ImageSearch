ddf07f84cf2525fbd5fb8d565d8128cc38bef2a0
IsThisLoss <kopturovdim@gmail.com>
2023-05-08 15:00:05 +0400

    remove extra css

--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -129,35 +129,3 @@ export default defineComponent({
   },
 });
 </script>
-
-
-<style scoped>
-.masonry { 
-  display: flex;
-  flex-flow: row wrap;
-  margin-left: -8px; /* Adjustment for the gutter */
-}
-
-.masonry-brick {
-  flex: auto;
-  height: 250px;
-  min-width: 150px;
-  margin: 0 8px 8px 0; /* Some gutter */
-}
-
-.masonry-brick {
-  &:nth-child(4n+1){
-     width: 250px;
-  }
-  &:nth-child(4n+2){
-     width: 325px;
-  }
-  &:nth-child(4n+3){
-     width: 180px;
-  }
-  &:nth-child(4n+4){
-     width: 380px;
-  }
-}
-
-</style>
